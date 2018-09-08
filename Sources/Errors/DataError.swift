a23105d80afb2b1ba8f86474499015bfc518538b
Basem Emara <contact@basememara.com>
2018-09-07 22:27:41 -0400

    Implement localization

--- Sources/Errors/DataError.swift
+++ Sources/Errors/DataError.swift
@@ -18,3 +18,23 @@ public enum DataError: Error {
     case networkFailure(Error?)
     case unknownReason(Error?)
 }
+
+public extension DataError {
+    
+    /// Get the localized description for this error
+    var localizedDescription: String {
+        switch self {
+        case .duplicateFailure: return .localized(.duplicateFailureErrorMessage)
+        case .nonExistent: return .localized(.nonExistentErrorMessage)
+        case .incomplete: return .localized(.genericIncompleteFormErrorMessage)
+        case .unauthorized: return .localized(.unauthorizedErrorMessage)
+        case .noInternet: return .localized(.noInternetErrorMessage)
+        case .timeout: return .localized(.serverTimeoutErrorMessage)
+        case .parseFailure: return .localized(.parseFailureErrorMessage)
+        case .databaseFailure: return .localized(.databaseFailureErrorMessage)
+        case .cacheFailure: return .localized(.cacheFailureErrorMessage)
+        case .networkFailure: return .localized(.networkFailureErrorMessage)
+        case .unknownReason: return .localized(.unknownReasonErrorMessage)
+        }
+    }
+}
---
+++ Sources/Extensions/Localizable.swift
@@ -0,0 +1,25 @@
+//
+//  Localizable.swift
+//  SwiftyPress
+//
+//  Created by Basem Emara on 2018-09-07.
+//
+
+import ZamzamKit
+
+// Errors
+public extension Localizable {
+    static let noInternetErrorMessage = Localizable(NSLocalizedString("no.internet.error.message", bundle: .swiftyPress, comment: "General error for no internet availability"))
+    static let serverTimeoutErrorMessage = Localizable(NSLocalizedString("server.timeout.error.message", bundle: .swiftyPress, comment: "General error for server availability"))
+    static let badNetworkRequestErrorMessage = Localizable(NSLocalizedString("bad.network.request.error.message", bundle: .swiftyPress, comment: "General error for bad network request"))
+    static let networkFailureErrorMessage = Localizable(NSLocalizedString("network.failure.error.message", bundle: .swiftyPress, comment: "General error for network server"))
+    static let parseFailureErrorMessage = Localizable(NSLocalizedString("parse.failure.error.message", bundle: .swiftyPress, comment: "General parse error for data"))
+    static let unknownReasonErrorMessage = Localizable(NSLocalizedString("unknown.reason.error.message", bundle: .swiftyPress, comment: "General error for unknown reason"))
+    
+    static let duplicateFailureErrorMessage = Localizable(NSLocalizedString("duplicate.failure.error.message", bundle: .swiftyPress, comment: "General error for duplicate data"))
+    static let nonExistentErrorMessage = Localizable(NSLocalizedString("non.existent.error.message", bundle: .swiftyPress, comment: "General error for non-existent data"))
+    static let unauthorizedErrorMessage = Localizable(NSLocalizedString("unauthorized.error.message", bundle: .swiftyPress, comment: "Unauthorized alert error message"))
+    static let genericIncompleteFormErrorMessage = Localizable(NSLocalizedString("generic.incomplete.form.error.message", bundle: .swiftyPress, comment: "Generic alert error message for incomplete form"))
+    static let databaseFailureErrorMessage = Localizable(NSLocalizedString("database.failure.error.message", bundle: .swiftyPress, comment: "General database error"))
+    static let cacheFailureErrorMessage = Localizable(NSLocalizedString("cache.failure.error.message", bundle: .swiftyPress, comment: "Cache storage failure even though remote succeeded"))
+}
