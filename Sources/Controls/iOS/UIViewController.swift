8853893e4962f359b2ebe8f494b7807d68acc0a2
Basem Emara <contact@basememara.com>
2018-10-08 16:24:29 -0400

    Extension for presenting styled Safari

---
+++ Sources/Controls/iOS/UIViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewController.swift
+//  SwiftyPress iOS
+//
+//  Created by Basem Emara on 2018-10-08.
+//
+
+import UIKit
+import ZamzamKit
+
+public extension UIViewController {
+    
+    /**
+     Open Safari view controller overlay.
+     
+     - parameter url: URL to display in the browser.
+     - parameter theme: The style of the Safari view controller.
+     */
+    func present(safari url: String, modalPresentationStyle: UIModalPresentationStyle = .overFullScreen,
+                 theme: Theme, animated: Bool = true, completion: (() -> Void)? = nil) {
+        present(
+            safari: url,
+            modalPresentationStyle: modalPresentationStyle,
+            barTintColor: theme.backgroundColor,
+            preferredControlTintColor: theme.tint,
+            animated: animated,
+            completion: completion
+        )
+    }
+}
