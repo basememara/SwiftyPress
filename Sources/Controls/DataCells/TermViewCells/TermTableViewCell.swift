a9cfe56121c52618ea540a1bc35159c4336ba493
Basem Emara <contact@basememara.com>
2018-10-22 21:37:40 -0400

    Promote data cells to core

---
+++ Sources/Controls/DataCells/TermViewCells/TermTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TagTableViewCell.swift
+//  Basem Emara
+//
+//  Created by Basem Emara on 2018-09-26.
+//  Copyright © 2018 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+
+open class TermTableViewCell: UITableViewCell, TermsDataViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+}
+
+extension TermTableViewCell {
+    
+    open func bind(_ model: TermsDataViewModel) {
+        nameLabel.text = model.name
+        countLabel.text = "(\(model.count))"
+    }
+}
