1000a2fe5df6c4bac1c61e858172b164e2ccd558
Basem Emara <contact@basememara.com>
2019-07-30 12:15:32 -0400

    Moved themed controls locally to project

---
+++ Sources/Styles/Controls/ThemedTextField.swift
@@ -0,0 +1,14 @@
+//
+//  ThemedTextField.swift
+//  ZamzamKit iOS
+//
+//  Created by Basem Emara on 2019-05-08.
+//  Copyright © 2019 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+import ZamzamKit
+
+open class ThemedTextField: NextResponderTextField {
+    
+}
---
+++ Sources/Styles/Controls/ThemedTextView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemedTextView.swift
+//  ZamzamKit iOS
+//
+//  Created by Basem Emara on 2019-05-08.
+//  Copyright © 2019 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+import ZamzamKit
+
+open class ThemedTextView: UITextView {
+    
+}
+
+open class ThemedLabelView: UILabelView {
+    
+}
