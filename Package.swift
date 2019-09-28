07ea23a8f7280f173d8cbc1ba8c6cc99c0a6e89b
Basem Emara <contact@basememara.com>
2019-09-27 21:55:04 -0400

    Update package manifest

--- Package.swift
+++ Package.swift
@@ -1,15 +1,14 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftyPress",
     platforms: [
-        .macOS(.v10_12),
+        .macOS(.v10_14),
         .iOS(.v11),
         .tvOS(.v11),
-        .watchOS(.v3)
+        .watchOS(.v4)
     ],
     products: [
         .library(
@@ -21,9 +20,8 @@ let package = Package(
         .package(url: "git@github.com:SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "1.7.0")),
         .package(url: "git@github.com:Alamofire/Alamofire.git", from: "5.0.0-rc.2"),
         .package(url: "git@github.com:realm/realm-cocoa.git", .upToNextMajor(from: "3.18.0")),
-        .package(url: "git@github.com:ZamzamInc/ZamzamNotification.git", .branch("master")),
-        .package(url: "git@github.com:ZamzamInc/ZamzamUI.git", .branch("develop")),
-        .package(url: "git@github.com:ZamzamInc/Stencil.git", .branch("lite")),
+        .package(url: "git@github.com:ZamzamInc/ZamzamKit.git", .branch("develop")),
+        .package(url: "git@github.com:stencilproject/Stencil.git", .upToNextMajor(from: "0.13.1")),
         .package(url: "git@github.com:onevcat/Kingfisher.git", .upToNextMajor(from: "5.8.1"))
     ],
     targets: [
