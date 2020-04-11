db5dee6de81ea9b6b923fd6e8d228de30bb8ec6c
Basem Emara <contact@basememara.com>
2020-04-10 22:27:34 -0400

    Fix Swift Package dependencies

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Realm", url: "git@github.com:realm/realm-cocoa.git", .upToNextMinor(from: "4.4.0")),
-        .package(url: "git@github.com:ZamzamInc/ZamzamKit.git", .upToNextMinor(from: "5.3.1")),
+        .package(url: "git@github.com:ZamzamInc/ZamzamKit.git", .upToNextMinor(from: "5.3.3")),
         .package(url: "git@github.com:ZamzamInc/Stencil.git", .upToNextMinor(from: "0.13.2")),
-        .package(url: "git@github.com:onevcat/Kingfisher.git", .revision("16e6ec9b42263adf6bdaa6ddb7716bd5236d7b63"))
+        .package(url: "git@github.com:onevcat/Kingfisher.git", .upToNextMinor(from: "5.13.4"))
     ],
     targets: [
         .target(
